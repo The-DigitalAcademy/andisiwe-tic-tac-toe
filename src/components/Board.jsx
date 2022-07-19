b24b9be09b459562c0ea3b8ede35a3cdadcb404c
DA M1 61 <DAM161@DAs-MacBook-48.local>
2022-07-19 15:45:40 +0200

    testing

---
+++ src/components/Board.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import {Box} from './Box'
+import "./Board.css"
+
+export const Board = ({board, onclick}) => {
+  
+  return (
+    <div className='board'>
+        {
+  board.map((value, idx) => {
+            return <Box value={value} key={idx} onClick={() => value === null && onclick(idx)} />;
+        })
+        }
+    </div>
+  )
+}
